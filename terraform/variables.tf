69161dbcd9a0e1f1939794d4e6ccd0a96535ed2f
Manuel Villanueva <mvillanueva.a@outlook.com>
2025-06-15 19:57:16 -0600

    Add Terraform configuration for local k3d cluster with Rancher

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "rancher_admin_password" {
+  description = "Admin password for the Rancher UI"
+  type        = string
+  sensitive   = true
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
